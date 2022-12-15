2885b4bdbbfe770233887cf726b497cc0a69217b
Nyx <nyxfor13days@gmail.com>
2022-12-15 20:42:12 +0530

    Removed Server From the App

--- server/utils/getCustomers.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const jsonData = require("../data/customers.json");
-
-const getCustomers = () => {
-  const customers = jsonData;
-  return customers;
-};
-
-module.exports = getCustomers;
